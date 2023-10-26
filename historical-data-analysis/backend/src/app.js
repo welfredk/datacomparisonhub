e14d035c3e305569a201da7f7da64e7b2d9d03fe
welfredk <welfredkng@gmail.com>
2023-10-26 14:00:27 +0800

    ready for deploy

--- historical-data-analysis/backend/src/app.js
+++ historical-data-analysis/backend/src/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan')
+
 const app = express();
+
 const { initialData } = require('./models/initialData.model')
 const { processHistoricalData } = require('./models/dataProcessor.model');
 const { availableCountry } = require('./models/availableCountry.model');
@@ -10,6 +13,7 @@ app.use(cors({
     origin: ['http://localhost:3000'],
     
 }));
+app.use(morgan('combined'));
 
 app.use(express.json());
 
